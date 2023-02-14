20e9b42fbc684cc25d21591711175c9ba53246a7
narvikd <84069271+narvikd@users.noreply.github.com>
2023-02-14 21:16:11 +0100

    main start
    Signed-off-by: narvikd <84069271+narvikd@users.noreply.github.com>

--- main.go
+++ main.go
@@ -3,21 +3,52 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"log"
+	"nubedb/api/proto/protoserver"
 	"nubedb/api/rest/middleware"
 	"nubedb/api/rest/route"
 	"nubedb/internal/app"
 	"nubedb/internal/config"
+	"sync"
 )
 
 func main() {
-	cfg := config.New()
-	errListen := newApi(cfg).Listen(cfg.ApiAddress)
+	start(config.New())
+}
+
+func start(cfg config.Config) {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		startApiRest(cfg)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		startApiProto(cfg)
+	}()
+
+	wg.Wait()
+}
+
+func startApiProto(cfg config.Config) {
+	log.Println("Starting proto server...")
+	err := protoserver.Start(cfg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func startApiRest(cfg config.Config) {
+	errListen := newApiRest(cfg).Listen(cfg.ApiAddress)
 	if errListen != nil {
 		log.Fatalln("api can't be started:", errListen)
 	}
 }
 
-func newApi(cfg config.Config) *fiber.App {
+func newApiRest(cfg config.Config) *fiber.App {
 	a := app.NewApp(cfg)
 	middleware.InitMiddlewares(a.HttpServer)
 	route.Register(a)
