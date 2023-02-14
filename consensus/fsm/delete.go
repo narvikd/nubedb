c575975f37d5b041d20b98554f3a2269005ed883
narvikd <84069271+narvikd@users.noreply.github.com>
2023-02-14 16:31:08 +0100

    Copy get logic into delete
    Signed-off-by: narvikd <84069271+narvikd@users.noreply.github.com>

--- consensus/fsm/delete.go
+++ consensus/fsm/delete.go
@@ -1,10 +1,33 @@
 package fsm
 
-import "github.com/narvikd/errorskit"
+import (
+	"errors"
+	"github.com/narvikd/errorskit"
+)
 
 func (dbFSM DatabaseFSM) delete(k string) error {
 	txn := dbFSM.db.NewTransaction(true)
 	defer txn.Discard()
+
+	dbResultValue := make([]byte, 0)
+	dbResult, errGet := txn.Get([]byte(k))
+	if errGet != nil {
+		return errGet
+	}
+
+	errDBResultValue := dbResult.Value(func(val []byte) error {
+		dbResultValue = append(dbResultValue, val...)
+		return nil
+	})
+
+	if errDBResultValue != nil {
+		return errDBResultValue
+	}
+
+	if dbResultValue == nil || len(dbResultValue) <= 0 {
+		return errors.New("no result for key")
+	}
+
 	err := txn.Delete([]byte(k))
 	if err != nil {
 		return err
