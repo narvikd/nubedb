97b4db3990df1bebfc990d62a7d5298afc1605a2
narvikd <84069271+narvikd@users.noreply.github.com>
2023-02-14 16:34:06 +0100

    Removed value logic from get
    Signed-off-by: narvikd <84069271+narvikd@users.noreply.github.com>

--- consensus/fsm/delete.go
+++ consensus/fsm/delete.go
@@ -1,7 +1,6 @@
 package fsm
 
 import (
-	"errors"
 	"github.com/narvikd/errorskit"
 )
 
@@ -9,28 +8,14 @@ func (dbFSM DatabaseFSM) delete(k string) error {
 	txn := dbFSM.db.NewTransaction(true)
 	defer txn.Discard()
 
-	dbResultValue := make([]byte, 0)
-	dbResult, errGet := txn.Get([]byte(k))
+	_, errGet := txn.Get([]byte(k))
 	if errGet != nil {
 		return errGet
 	}
 
-	errDBResultValue := dbResult.Value(func(val []byte) error {
-		dbResultValue = append(dbResultValue, val...)
-		return nil
-	})
-
-	if errDBResultValue != nil {
-		return errDBResultValue
-	}
-
-	if dbResultValue == nil || len(dbResultValue) <= 0 {
-		return errors.New("no result for key")
-	}
-
-	err := txn.Delete([]byte(k))
-	if err != nil {
-		return err
+	errDelete := txn.Delete([]byte(k))
+	if errDelete != nil {
+		return errDelete
 	}
 
 	errCommit := txn.Commit()
