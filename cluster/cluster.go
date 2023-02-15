a0449ca7ca8967e7b39d3435da263d401bcdbd77
narvikd <84069271+narvikd@users.noreply.github.com>
2023-02-15 15:05:52 +0100

    Refactor forwardLeaderFuture to make it work correctly
    Signed-off-by: narvikd <84069271+narvikd@users.noreply.github.com>

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -1,24 +1,28 @@
 package cluster
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"github.com/hashicorp/raft"
 	"github.com/narvikd/errorskit"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"nubedb/api/proto"
 	"nubedb/cluster/consensus/fsm"
+	"nubedb/internal/config"
 	"time"
 )
 
-const errNodeNotALeader = "node is not a leader"
-
-func Execute(consensus *raft.Raft, payload *fsm.Payload) error {
+func Execute(cfg config.Config, consensus *raft.Raft, payload *fsm.Payload) error {
 	payloadData, errMarshal := json.Marshal(&payload)
 	if errMarshal != nil {
 		return errorskit.Wrap(errMarshal, "couldn't marshal data to send it to the DB cluster")
 	}
 
 	if consensus.State() != raft.Leader {
-		return errors.New(errNodeNotALeader)
+		return handleForwardLeaderFuture(cfg, consensus, payload)
 	}
 
 	return applyLeaderFuture(consensus, payloadData)
@@ -40,31 +44,40 @@ func applyLeaderFuture(consensus *raft.Raft, payloadData []byte) error {
 	return nil
 }
 
-//func forwardsLeaderFuture(consensus *raft.Raft, payloadData []byte) error {
-//	// Regex for port: /(?<=:)\d+/
-//
-//	//log.Printf("[proto] payload for leader received in this node, forwarding to leader '%s' @ '%s'\n",
-//	//	leaderID, leaderAddress,
-//	//)
-//
-//	const errGrpcTalk = "failed to get an ok response from the Leader via grpc"
-//
-//	conn, errDial := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-//	if errDial != nil {
-//		return errorskit.Wrap(errDial, "failed to connect to the Leader via grpc")
-//	}
-//	defer conn.Close()
-//
-//	client := proto.NewServiceClient(conn)
-//	res, errTalk := client.ExecuteOnLeader(context.Background(), &proto.Request{
-//		Payload: payloadData,
-//	})
-//	if errTalk != nil {
-//		return errorskit.Wrap(errTalk, errGrpcTalk)
-//	}
-//	if res.Error != "" {
-//		return errors.New(errGrpcTalk)
-//	}
-//
-//	return nil
-//}
+func handleForwardLeaderFuture(cfg config.Config, consensus *raft.Raft, payload *fsm.Payload) error {
+	_, leaderID := consensus.LeaderWithID()
+	leaderCfg := cfg.Nodes[string(leaderID)]
+	return forwardLeaderFuture(leaderCfg, payload)
+}
+
+func forwardLeaderFuture(leaderCfg config.NodeCfg, payload *fsm.Payload) error {
+	log.Printf("[proto] payload for leader received in this node, forwarding to leader '%s' @ '%s'\n",
+		leaderCfg.ID, leaderCfg.GrpcAddress,
+	)
+
+	payloadData, errMarshal := json.Marshal(&payload)
+	if errMarshal != nil {
+		return errorskit.Wrap(errMarshal, "couldn't marshal data to send it to the Leader's DB cluster")
+	}
+
+	const errGrpcTalk = "failed to get an ok response from the Leader via grpc"
+
+	conn, errDial := grpc.Dial(leaderCfg.GrpcAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if errDial != nil {
+		return errorskit.Wrap(errDial, "failed to connect to the Leader via grpc")
+	}
+	defer conn.Close()
+
+	client := proto.NewServiceClient(conn)
+	res, errTalk := client.ExecuteOnLeader(context.Background(), &proto.Request{
+		Payload: payloadData,
+	})
+	if errTalk != nil {
+		return errorskit.Wrap(errTalk, errGrpcTalk)
+	}
+	if res.Error != "" {
+		return errors.New(errGrpcTalk)
+	}
+
+	return nil
+}
