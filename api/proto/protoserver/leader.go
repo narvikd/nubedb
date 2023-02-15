9e1653c6c272975c3f0d10f3892d6c068fe755bd
narvikd <84069271+narvikd@users.noreply.github.com>
2023-02-15 02:02:08 +0100

    Change behaviour to execute based on struct instead of bytes
    Signed-off-by: narvikd <84069271+narvikd@users.noreply.github.com>

--- api/proto/protoserver/leader.go
+++ api/proto/protoserver/leader.go
@@ -2,18 +2,30 @@ package protoserver
 
 import (
 	"context"
+	"encoding/json"
+	"github.com/narvikd/errorskit"
 	"log"
 	"nubedb/api/proto"
 	"nubedb/cluster"
+	"nubedb/cluster/consensus/fsm"
 )
 
-func (srv *server) LeaderApplyFuture(ctx context.Context, req *proto.Request) (*proto.Response, error) {
+func (srv *server) ExecuteOnLeader(ctx context.Context, req *proto.Request) (*proto.Response, error) {
 	log.Println("[proto] request received")
-	err := cluster.ApplyLeaderFuture(srv.Consensus, req.Payload)
-	if err != nil {
+
+	p := new(fsm.Payload)
+	errUnmarshal := json.Unmarshal(req.Payload, p)
+	if errUnmarshal != nil {
 		return &proto.Response{
-			Error: err.Error(),
-		}, err
+			Error: errorskit.Wrap(errUnmarshal, "couldn't unmarshal payload").Error(),
+		}, errUnmarshal
+	}
+
+	errExecute := cluster.Execute(srv.Consensus, p)
+	if errExecute != nil {
+		return &proto.Response{
+			Error: errExecute.Error(),
+		}, errExecute
 	}
 
 	log.Println("[proto] request successful")
