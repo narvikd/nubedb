281b64cba6f6469bf3a1a3223cf8f37eae5f2948
narvikd <84069271+narvikd@users.noreply.github.com>
2023-02-16 15:28:58 +0100

    chore: Simplify jsonresponse
    Signed-off-by: narvikd <84069271+narvikd@users.noreply.github.com>

--- api/rest/jsonresponse/jsonresponse.go
+++ api/rest/jsonresponse/jsonresponse.go
@@ -2,7 +2,7 @@ package jsonresponse
 
 import "github.com/gofiber/fiber/v2"
 
-func Make(ctx *fiber.Ctx, status int, success bool, message string, data interface{}) error {
+func Make(ctx *fiber.Ctx, status int, success bool, message string, data any) error {
 	return ctx.Status(status).JSON(&fiber.Map{
 		"success": success,
 		"message": message,
@@ -11,9 +11,8 @@ func Make(ctx *fiber.Ctx, status int, success bool, message string, data interfa
 }
 
 // OK returns a successful response with status code 200
-func OK(ctx *fiber.Ctx, message string, data interface{}) error {
+func OK(ctx *fiber.Ctx, message string, data any) error {
 	return ctx.Status(200).JSON(&fiber.Map{
-		"success": true,
 		"message": message,
 		"data":    data,
 	})
@@ -22,26 +21,20 @@ func OK(ctx *fiber.Ctx, message string, data interface{}) error {
 // NotFound returns a not found response with status code 404
 func NotFound(ctx *fiber.Ctx, message string) error {
 	return ctx.Status(404).JSON(&fiber.Map{
-		"success": false,
 		"message": message,
-		"data":    "",
 	})
 }
 
 // BadRequest returns a bad request response with status code http status 400
 func BadRequest(ctx *fiber.Ctx, message string) error {
 	return ctx.Status(400).JSON(&fiber.Map{
-		"success": false,
 		"message": message,
-		"data":    "",
 	})
 }
 
 // ServerError returns a server error response with status code 500
 func ServerError(ctx *fiber.Ctx, message string) error {
 	return ctx.Status(500).JSON(&fiber.Map{
-		"success": false,
 		"message": message,
-		"data":    "",
 	})
 }
