b65580066ac7a661541cca3a7a0dbc9c2c70cbb6
narvikd <84069271+narvikd@users.noreply.github.com>
2023-02-20 01:04:31 +0100

    Add comments
    Signed-off-by: narvikd <84069271+narvikd@users.noreply.github.com>

--- api/proto/protoclient/protoclient.go
+++ api/proto/protoclient/protoclient.go
@@ -1,3 +1,4 @@
+// Package protoclient provides a client to connect to nubedb's gRPC server.
 package protoclient
 
 import (
@@ -9,12 +10,12 @@ import (
 	"time"
 )
 
-type CleanFunc func()
-
+// ConnCleaner is an interface for cleaning up connections.
 type ConnCleaner interface {
 	Cleanup()
 }
 
+// Connection represents a connection to a gRPC server.
 type Connection struct {
 	Conn          *grpc.ClientConn
 	Client        proto.ServiceClient
@@ -22,11 +23,21 @@ type Connection struct {
 	cancelCtxCall context.CancelFunc
 }
 
+// Cleanup closes the connection and cancels the context.
 func (c *Connection) Cleanup() {
 	_ = c.Conn.Close()
 	c.cancelCtxCall()
 }
 
+// NewConnection creates a new connection to a gRPC server.
+//
+// Example:
+//
+//	conn, errConn := protoclient.NewConnection(leaderGrpcAddr)
+//	if errConn != nil {
+//		return errConn
+//	}
+//	defer conn.Cleanup()
 func NewConnection(addr string) (*Connection, error) {
 	const (
 		dialTimeout       = 1 * time.Second
@@ -34,20 +45,23 @@ func NewConnection(addr string) (*Connection, error) {
 		errGrpcConnection = "grpc connection failed"
 	)
 
+	// Create a context with a timeout for dialing the server.
 	ctxDial, cancelCtxDial := context.WithTimeout(context.Background(), dialTimeout)
 	defer cancelCtxDial()
 
+	// Dial the server using the context.
 	connDial, errDial := grpc.DialContext(ctxDial, addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if errDial != nil {
 		return nil, errorskit.Wrap(errDial, errGrpcConnection)
 	}
 
+	// Create a context with a timeout for executing gRPC calls.
 	ctxExecuteCall, cancelCtxExecuteCall := context.WithTimeout(context.Background(), timeoutGrpcCall)
 
 	return &Connection{
-		Conn:          connDial,
-		Client:        proto.NewServiceClient(connDial),
-		Ctx:           ctxExecuteCall,
-		cancelCtxCall: cancelCtxExecuteCall,
+		Conn:          connDial,                         // set the gRPC connection
+		Client:        proto.NewServiceClient(connDial), // create a new service client
+		Ctx:           ctxExecuteCall,                   // set the context for executing gRPC calls
+		cancelCtxCall: cancelCtxExecuteCall,             // set the cancel function for the context included before
 	}, nil
 }
--- api/proto/protoserver/protoserver.go
+++ api/proto/protoserver/protoserver.go
@@ -1,3 +1,4 @@
+// Package protoserver provides nubedb's gRPC server implementation.
 package protoserver
 
 import (
@@ -9,25 +10,28 @@ import (
 	"nubedb/internal/config"
 )
 
+// server represents the gRPC server.
 type server struct {
 	proto.UnimplementedServiceServer
 	Config config.Config
 	Node   *consensus.Node
 }
 
+// Start starts the gRPC server.
 func Start(a *app.App) error {
 	listen, errListen := net.Listen("tcp", a.Config.CurrentNode.GrpcAddress)
 	if errListen != nil {
 		return errListen
 	}
 
+	// Create the server model.
 	srvModel := &server{
 		Config: a.Config,
 		Node:   a.Node,
 	}
 
 	protoServer := grpc.NewServer()
-	proto.RegisterServiceServer(protoServer, srvModel)
+	proto.RegisterServiceServer(protoServer, srvModel) // register the server model
 
 	return protoServer.Serve(listen)
 }
